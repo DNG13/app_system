1eccc2d6eaf3acc6150843ca97f1103bdcfba7c8
DNG13 <nataljasimple@gmail.com>
2017-11-27 14:55:18 +0200

    favicon added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Khanifest</title>
-
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <!-- Styles -->
     <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
