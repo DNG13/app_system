13d0e439e3bf982243fb6cc09b8427be83fafeb8
DNG13 <nataljasimple@gmail.com>
2017-12-10 15:36:54 +0200

    Register  avatar in Facebook enter changed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -165,22 +165,24 @@ class RegisterController extends Controller
 
             $user_id = $user->id;
 
-            $avatar = new Avatar();
-            $avatar->user_id = $user_id;
-            $socialUser->getAvatar();
-            $fileContents = file_get_contents($socialUser->getAvatar());
-            $imageName =  $user_id . '_'.uniqid() .'.' . ".jpg";
-            File::put(public_path() . '/uploads/avatars/' .  $imageName, $fileContents);
-            $avatar->link = 'uploads/avatars/'.$imageName;
-            $avatar->name = $imageName;
-            $img = Image::make($avatar->link);
-            $img->resize(null, 100, function ($constraint) {
-                $constraint->aspectRatio();
-            });
-            $img->save();
-            $avatar->save();
-
-            $avatar_id = $avatar->id;
+            if($socialUser->getAvatar()) {
+                $avatar = new Avatar();
+                $avatar->user_id = $user_id;
+                $fileContents = file_get_contents($socialUser->getAvatar());
+                $imageName = $user_id . '_' . uniqid() . '.' . ".jpg";
+                File::put(public_path() . '/uploads/avatars/' . $imageName, $fileContents);
+                $avatar->link = 'uploads/avatars/' . $imageName;
+                $avatar->name = $imageName;
+                $img = Image::make($avatar->link);
+                $img->resize(null, 100, function ($constraint) {
+                    $constraint->aspectRatio();
+                });
+                $img->save();
+                $avatar->save();
+                $avatar_id = $avatar->id;
+            } else {
+                $avatar_id = null;
+            }
 
             $profile = new Profile();
             $profile->user_id = $user_id;
