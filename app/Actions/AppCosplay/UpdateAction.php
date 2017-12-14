0c796c5d0bedfd485ba869bb613bb11cf293dd88
IGK <iigkey@gmail.com>
2017-12-14 21:37:03 +0200

    added action separated

---
+++ app/Actions/AppCosplay/UpdateAction.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Actions\AppCosplay;
+
+use App\Abstracts\Action;
+use App\Models\AppCosplay;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+
+class UpdateAction extends Action
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function run(Request $request, $id)
+    {
+
+        //store in database
+        $cosplays = AppCosplay::where('id', $id)->first();
+        $cosplays->type_id = $request->get('type_id');
+        $cosplays->title = $request->get('title');
+        $cosplays->fandom = $request->get('fandom');
+        $cosplays->length = $request->get('length');
+        $cosplays->city = $request->get('city');
+        $cosplays->description = $request->get('description');
+        $cosplays->prev_part = $request->get('prev_part');
+        $cosplays->comment = $request->get('comment');
+        if($cosplays->status != $request->get('status')) {
+            $user =  User::where('id', $cosplays->user_id)->first();
+            $mail['nickname'] = $user->profile->nickname;
+            $mail['title'] = $cosplays->title;
+            $mail['email'] = $user->email;
+            $mail['page'] ='/cosplay/'. $cosplays->id;
+            $mail['status'] = $request->get('status');
+            Mail::send('mails.status',  $mail , function($message) use ( $mail ){
+                $message->to( $mail['email']);
+                $message->subject('Изминение статуса завки');
+            });
+        }
+        if($request->get('status')) {
+            if (Auth::user()->isAdmin()) {
+                $cosplays->status = $request->get('status');
+            }
+        }
+        $members = [];
+        foreach($request->input('members') as  $key => $value) {
+            $members["member{$key}"] = $value;
+        }
+        $cosplays->members_count = count($members);
+        $cosplays->members = json_encode($members);
+        $cosplays->save();
+        if (!Auth::user()->isAdmin()) {
+            $mail['nickname'] = 'Admin';
+            $mail['email'] = 'khanifest.mail@gmail.com';
+            $mail['title'] = $cosplays->title;
+            $mail['page'] = '/cosplay/'. $cosplays->id;
+            Mail::send('mails.edit', $mail, function ($message) use ($mail) {
+                $message->to($mail['email']);
+                $message->subject('Заявка ' .$mail['title'] . ' изменена');
+            });
+        }
+        return redirect('cosplay')->with('success', "Ваша заявка успешно изменена.");
+    }
+}
--- app/Http/Controllers/AppCosplayController.php
+++ app/Http/Controllers/AppCosplayController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\AppCosplay\UpdateAction;
 use App\Http\Requests\AppCosplay\IndexRequest;
 use App\Models\AppType;
 use App\Models\AppCosplay;
@@ -184,13 +185,12 @@ class AppCosplayController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param    $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param $id
+     * @param UpdateAction $action
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $id, UpdateAction $action)
     {
         //validate the data
         $this->validate($request,[
@@ -203,51 +203,8 @@ class AppCosplayController extends Controller
             'prev_part' => '',
             'comment' => '',
         ]);
-        //store in database
-        $cosplays = AppCosplay::where('id', $id)->first();
-        $cosplays->type_id = $request->get('type_id');
-        $cosplays->title = $request->get('title');
-        $cosplays->fandom = $request->get('fandom');
-        $cosplays->length = $request->get('length');
-        $cosplays->city = $request->get('city');
-        $cosplays->description = $request->get('description');
-        $cosplays->prev_part = $request->get('prev_part');
-        $cosplays->comment = $request->get('comment');
-        if($cosplays->status != $request->get('status')) {
-            $user =  User::where('id', $cosplays->user_id)->first();
-            $mail['nickname'] = $user->profile->nickname;
-            $mail['title'] = $cosplays->title;
-            $mail['email'] = $user->email;
-            $mail['page'] ='/cosplay/'. $cosplays->id;
-            $mail['status'] = $request->get('status');
-            Mail::send('mails.status',  $mail , function($message) use ( $mail ){
-                $message->to( $mail['email']);
-                $message->subject('Изминение статуса завки');
-            });
-        }
-        if($request->get('status')) {
-            if (Auth::user()->isAdmin()) {
-                $cosplays->status = $request->get('status');
-            }
-        }
-        $members = [];
-        foreach($request->input('members') as  $key => $value) {
-            $members["member{$key}"] = $value;
-        }
-        $cosplays->members_count = count($members);
-        $cosplays->members = json_encode($members);
-        $cosplays->save();
-        if (!Auth::user()->isAdmin()) {
-            $mail['nickname'] = 'Admin';
-            $mail['email'] = 'khanifest.mail@gmail.com';
-            $mail['title'] = $cosplays->title;
-            $mail['page'] = '/cosplay/'. $cosplays->id;
-            Mail::send('mails.edit', $mail, function ($message) use ($mail) {
-                $message->to($mail['email']);
-                $message->subject('Заявка ' .$mail['title'] . ' изменена');
-            });
-        }
-        return redirect('cosplay')->with('success', "Ваша заявка успешно изменена.");
+
+        return $action->run($request, $id);
     }
 
     /**
