530fe734e4cfca714fafa0ccee4bed5af158be2a
DNG13 <nataljasimple@gmail.com>
2017-12-28 21:55:43 +0200

    dropzone added to git

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -16,15 +16,8 @@ class RolesTableSeeder extends Seeder
             'key' => 'admin',
             'title' => 'Координаторы фестиваля',
             'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now(),
             'active' => true,
         ]);
-        Role::create([
-            'key' => 'user',
-            'title' =>'Пользователи',
-            'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now(),
-            'active' => true,
-        ]);
+
     }
 }
